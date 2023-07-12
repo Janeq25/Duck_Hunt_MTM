b6e6d01b0f992a47d131f8dd43580e175cb7d864
ArKur2001 <arekkurnik006@wp.pl>
2023-07-12 11:39:49 +0200

    rng_files_added

---
+++ rtl/rng/flip_flop.sv
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2023  AGH University of Science and Technology
+ * MTM UEC2
+ * Author: Arkadiusz Kurnik
+ * 
+ * Description:
+ * flip-flop module
+ */
+
+`timescale 1 ns / 1 ps
