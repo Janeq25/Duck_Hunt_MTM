d11961fb18c1e3a9cbdd2a73108e6abe35cbc203
ArKur2001 <arekkurnik006@wp.pl>
2023-07-18 10:01:25 +0200

    ctl_score first commit

---
+++ rtl/ctl/ctl_score.sv
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) 2023  AGH University of Science and Technology
+ * MTM UEC2
+ * Author: Arkadiusz Kurnik
+ * 
+ * Description:
+ * calculates score for 7seg display
+ */
+
+ `timescale 1 ns / 1 ps
+
+ module ctl_score(
+
+ );
+
+ endmodule
